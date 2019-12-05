6753a6af76b552b77d4527e408456bc597ca533c
Tyson Knutson <tyson.knutson@gmail.com>
2019-12-05 09:52:02 -0600

    adding controllers

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,50 @@
+using System;
+using Login.Models;
+using Login.Processors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Login.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ILogger<AdminController> _logger;
+        private readonly DatabaseManager _databaseManager;
+        private readonly AppDb _db;
+
+        public AdminController(ILogger<AdminController> logger, DatabaseManager databaseManager, AppDb db)
+        {
+            _logger = logger;
+            _databaseManager = databaseManager;
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            //Get existing users
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task AddUser(User user)
+        {
+            //save user
+            
+            // await _db.Connection.OpenAsync();
+            var dbMgr = new DatabaseManager(_db);
+            await dbMgr.InsertAsync(user);
+            
+            //redirect to index
+            Redirect("Admin/Index");
+            //return View();
+        }
+
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -8,12 +8,15 @@ namespace Login.Models
     {
         [Required]
         [Display(Name = "User name")]
-        public string UserName { get; set; }
+        public string UserName { get; set; }        
  
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
+
+        [Display(Name = "Phone Number")]
+        public string Phone { get; set; }
  
         [Display(Name = "Remember on this computer")]
         public bool RememberMe { get; set; }
