f7643b7fc8d261db10ed7ff370b3de7b46bcf929
Tyson Knutson <tyson.knutson@gmail.com>
2019-12-05 09:47:27 -0600

    MySql connection working.  Can add users

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Login.Processors;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -24,6 +20,10 @@ namespace Login
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            
+            var appDb = new AppDb(Configuration["ConnectionStrings:DefaultConnection"]);
+            services.AddTransient<AppDb>(_ => appDb);
+            services.AddTransient<DatabaseManager>(_ => new DatabaseManager(appDb));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
